880e75fe31672950253537a60dab345cf6b42711
hanwei <hanwei@ipaynow.cn>
2022-09-05 14:24:53 +0800

    缩进

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def get_baby_birth_count():
     return "芃芃来到这个世界已经 %d天啦!" %(count)
   else:
     count = (baby_date-today).days
-	return "芃芃还有 %d 天来到我们身边!嘻嘻(#^.^#)" %(count)
+    return "芃芃还有 %d 天来到我们身边!嘻嘻(#^.^#)" %(count)
 
 # 各种倒计时
 def get_counter_left(aim_date):
